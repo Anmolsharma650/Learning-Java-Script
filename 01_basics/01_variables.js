2c5abf5c42d964f4631ecb61ac7f4ed44b2b2a92
Anmolsharma650 <anmolsharma9896957756@gmail.com>
2025-12-24 17:49:48 +0530

    push

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountID = 43525
+let accountEmail = "anmolsharma989695756@gmail.com"
+var accountPassword = "435252"
+accountCity1 = "gurugram"
+accountCity = "panipat"
+
+
+
+console.table([accountID,accountEmail,accountPassword,accountCity,accountCity1])
---
+++ 01_basics/02_datatypes_ecmastanderds.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all js code a new versiom
+
+//alert( 3 + 3 ) //we are using node js,not browser
+
+let name = "anmol sharma" // this datatype called string
+let age = 22 // this is called number datatype
+let isLoggedIn = false // this is called boolean datatype
+
+console.log(typeof null);
+
+// numbers => 2 to power 53
+// bgint =>  ussing big project or big e-commarce website like linldin,facebook
+//string =>" something you wants"
+//boolean => true/false statment 
+// null => standlone value
+//undefiend => means undefiend
+// symbol => uniqe specification
+// object
