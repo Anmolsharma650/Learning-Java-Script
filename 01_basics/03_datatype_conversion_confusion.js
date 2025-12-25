98a1af3bcd0d3e22c68f1472b6a845318358a5d9
Anmolsharma650 <anmolsharma9896957756@gmail.com>
2025-12-25 16:34:56 +0530

    push

--- 01_basics/03_datatype_conversion_confusion.js
+++ 01_basics/03_datatype_conversion_confusion.js
@@ -10,7 +10,7 @@ let valueInNumber = Number(score)
 let isLoggedIn = ""
 
 //let booleanIsLoggedIn = Boolean(isLoggedIn)
-  //console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -19,6 +19,22 @@ let isLoggedIn = ""
 let someNumber = 33
 
 let stringNumber = String(someNumber)
- console.log(stringNumber);
- console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
+
+
+//operation value
+
+let str1 = "anmol"
+let str2 = 4 
+let str3 = str1 + str2
+
+//console.log(str3);
+let x = 3;
+const y = x++;
+
+//console.log(`x:${x}, y:${y}`);
+for (let i = 0; i < 10; i++) {
+  //console.log(i);
+}
