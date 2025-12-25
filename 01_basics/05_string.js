f51c16b8dc877ea1dec651e2a614d95ac2c46034
Anmolsharma650 <anmolsharma9896957756@gmail.com>
2025-12-25 18:40:31 +0530

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,34 @@
+// const name = "Anmol"
+//const repoCount = 10
+
+//console.log(name + repoCount + " chai ");
+
+// console.log(` my name is ${name} and my repocount is ${repoCount}`);
+
+
+//const gameName = new String('hitesh-hc-com')
+
+//console.log(gameName[0]);
+//console.log(gameName.__proto__);
+
+
+//console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// onsole.log(gameName.indexOf('t'));
+
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString);
+
+ //console.log(newStringOne);
+ //console.log(newStringOne.trim());
+ //const newStringOne = "   hitesh    "
+
+ const url = "https://anmolsharma.com/anmol984sharma"
+ console.log( url.replace('984','-'));
+ 
+
+
