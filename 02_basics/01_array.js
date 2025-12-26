fc7b2b816a723f6933968af462d2aa3954369c87
Anmolsharma650 <anmolsharma9896957756@gmail.com>
2025-12-26 19:43:20 +0530

    arrays

---
+++ 02_basics/01_array.js
@@ -0,0 +1,33 @@
+//const myArray = [0,1,2,3,4]
+//const myHeroes = ['ironman','batman','padman']
+//const myarray = new Array(1,3,5,7)
+//console.log(myHeroes);
+//console.log(myHeroes[2]);
+
+
+/// Array Methods ///
+//myHeroes.push('krish ka gaana sunega')
+//myHeroes.pop()
+//myHeroes.unshift('krish ka gaana sunega')
+//myHeroes.shift()
+//console.log(myHeroes)
+
+//const newHeroes = myHeroes.join() 
+//console.log(newHeroes)
+//console.log(myHeroes)
+
+
+
+//let fruits = ["Apple", "Banana", "Mango", "Orange", "Grapes"];
+
+//let selectedFruits = fruits.slice(1, 3);
+
+//console.log(selectedFruits); 
+//console.log(fruits); 
+
+let fruits = ["Apple", "Banana", "Mango", "Orange", "Grapes"];
+
+let selectedFruits = fruits.splice(1, 3);
+
+console.log(selectedFruits); 
+console.log(fruits); 
