edb1d332e82358fd812020bf038ee075329df527
Anmolsharma650 <anmolsharma9896957756@gmail.com>
2025-12-29 20:59:49 +0530

    function

---
+++ 03_basics/01_function.js
@@ -0,0 +1,19 @@
+// function addTwoNumbers(number1,number2){
+//     console.log(number1+number2);
+
+// }
+//addTwoNumbers(245,5)
+
+// function addTwoNumbers(number1,number2){
+//     let result = number1 + number2 
+//     return result
+
+// }
+// const result = addTwoNumbers(245,5)
+//console.log("result :" ,result);
+
+function newLoggedIn (username)
+{
+return`${username} just logged in`
+}
+console.log(newLoggedIn("anmol"));
