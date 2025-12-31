3bad96435cd705e84526fc5769039d60ffa129e3
Anmolsharma650 <anmolsharma9896957756@gmail.com>
2025-12-31 18:10:38 +0530

    opretors

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -7,3 +7,44 @@
 
 
 // condition =    >,<,==,=>,<=,===,!=
+
+
+// const score = 200
+// if (score>100) {
+//     const power = "fly"
+//     console.log(`user power:${power}`);
+    
+    
+// }
+
+// const balance = 20000
+// //if (balance >10000) console.log("test");// implecet scope maan lena ki yaha scpoe hai
+// if (balance > 10000) {
+
+//     console.log(`balance reamninig: ${10000}`);
+// }
+// else if (balance > 15000) {
+   
+//     console.log(`balance reamninig: ${10000}`);
+    
+// }
+// else if (balance > 20000) {
+    
+//     console.log(`balance reamninig: ${10000}`);
+    
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoggle = true
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard) {
+    console.log("continue to buy something");
+
+}
+
+if (loggedInFromEmail ||  loggedInFromGoggle) {
+     console.log("user loggedin");
+    
+}
---
+++ 04_control_flow/truthy_values.js
@@ -0,0 +1,16 @@
+// const userEmail = "a@anmol.goggle"
+// const userEmail2 =""
+// if (userEmail2) {
+//     console.log("yes we got email id");
+// }else{
+//     console.log("no we have not email id");
+    
+// }
+
+//falsy values =>  nan, "", undifind , false, null, 0, -0, BigInt on ;
+
+ //nullish coalescing operator (??): null undefined
+
+ let val1;
+ val = 5 ?? 10
+console.log(val);
