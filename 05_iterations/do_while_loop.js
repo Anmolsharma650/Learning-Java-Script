6d7b631835c27ce8a6b47f363ce339e043899b72
Anmolsharma650 <anmolsharma9896957756@gmail.com>
2025-12-31 19:50:25 +0530

    loops

---
+++ 05_iterations/do_while_loop.js
@@ -0,0 +1,16 @@
+// let  index = 0
+// while (index <= 10) {
+//     console.log(`value of index ${index}`);
+//     index = index + 2
+    
+    
+// }
+
+let myArray = ["flash","ironman","thor"]
+let arr = 0
+while (arr <myArray.length) {
+    console.log(`value of arr is :${myArray[arr]}`);
+    arr = arr + 1
+    
+    
+}
---
+++ 05_iterations/for_loop.js
@@ -0,0 +1,50 @@
+// for (let i = 10; i >= 0; i--) {
+//     const element = i;
+//     if (element == 5) {
+//         console.log("print 5 is the best number");
+
+
+//     }
+//     console.log(element);
+
+//  }
+
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     if (element == 5) {
+//         console.log("print 5 is the best number");
+
+
+//     }
+//     console.log(element);
+
+//  }
+
+
+// for (let i = 1; i <= 10; i++) {
+
+// console.log(`Table of : ${i}`);
+
+
+//     for (let j = 1; j <= 10; j++) {
+
+//         console.log(i + '*' + j + '=' + i*j);
+//     }
+
+
+// }
+
+// const myArray = ["anmol","kamal","ansh","vansh","himanshu","priyanshu","ankit malpuriya","gay chirag "]
+// for (let index = 1; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+
+// }
+
+for (let i = 1; i < 10; i++) {
+    if (i == 5) {
+        console.log(" value detected 5");
+        break
+    } 
+    console.log(`value of i is:${i}`);
+}
